6669fc3a9513e73876dfbc242b29d657603f12ed
Xvn Anony <62823140+XvnAnony@users.noreply.github.com>
2020-04-09 06:33:01 +0700

    Add files via upload
    Git clone https://github.com/XvnAnony/Demonizedv2
    cd Demonizedv2
    sh Demonizedv2.sh

---
+++ Demonizedv2.sh
@@ -0,0 +1,331 @@
+#Ouh Mau Recode :( Ya Udah Gpp Yang Penting Dosa Di Tanggung Sendiri:)
+#Ya Udah Sana Ubah Author
+#Kirain Hacker Bikin Sc Sendiri Tapi Enggak:)
+#official Xvn Anony
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo $red
+figlet "Loading.."
+echo $red"Official Xvn Anony"
+sleep 3
+clear
+echo
+echo $green
+figlet "Demonized"
+echo "deafult tools on termux V2"
+echo
+echo $green"<================================================>"
+echo $white"Author : Xvn Anony"
+echo "Version: 2"
+echo "youtube: Xvn Anony"
+echo "github :https://github.com/XvnAnony"
+echo $green"<================================================>"
+sleep 1
+echo
+echo $green"Silahkan Dipilih Bosq"
+echo
+sleep 2
+echo $green"<================================================>"
+echo $white"1) Mengotak Atik atau Menghapus data File Termux"
+echo $green"<================================================>"
+echo $white"2) Menampilkan Animasi Text Berjatuhan"
+echo $green"<================================================>"
+echo $white"3) Menampilkan Animasi Text Kereta Berjalan"
+echo $green"<================================================>"
+echo $white"4) Menampilkan Animasi Text Api Membakar"
+echo $green"<================================================>"
+echo $white"5) Chatingan Dengan Sesama Hacker Di Termux"
+echo $green"<================================================>"
+echo $white"6) Stabilkan Jaringan Internet Mu"
+echo $green"<================================================>"
+echo $white"7) Spam Mantan:v Atau Spam Orang Lain"
+echo $green"<================================================>"
+echo $white"8) Bikin Virtex Whats App"
+echo $green"<================================================>"
+echo $white"9) Hack Facebook Orang (no root)"
+echo $green"<================================================>"
+echo $white"10) Hack Instagram auto like (no root)"
+echo $green"<================================================>"
+echo $white"11) Pindah Script Tool-N"
+echo $green"<================================================>"
+echo $white"12) Ddos Website Pake Termux (no root) LITEDDOS"
+echo $green"<================================================>"
+echo $white"13) Pindah Script Bajinganv5"
+echo $green"<================================================>"
+echo $white"14) Menampilkan Data Data Tentang Hp Kentang Mu:v"
+echo $green"<================================================>"
+echo $white"15) Main Tebak Tebak An Biar Gak Galau:V"
+echo $green"<================================================>"
+echo $white"16) pindah Script Bajinganv6"
+echo $green"<================================================>"
+echo $white"17) Pindah Dan Install Script Dark fly"
+echo $green"<================================================>"
+echo $white"18) Mencari Kelemahan Website (No root)"
+echo $green"<================================================>"
+echo $white"19) Bikin Sc Deface (No Root) LITESCRIPT"
+echo $green"<================================================>"
+echo $white"0) Keluar Dari Tools:("
+echo $green"<================================================>"
+
+read -p "Silahkan Pilih No Berapa : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ] 
+then
+clear
+figlet "Loading.."
+sleep 2
+pkg update && pkg upgrade
+pkg install mc
+mc
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+figlet "Loading.."
+sleep 2
+pkg update && pkg upgrade
+pkg install cmatrix
+cmatrix
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet "Loading.."
+sleep 2
+pkg update && pkg upgrade
+pkg install sl
+sl
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+figlet "Loading.."
+sleep 2
+pkg update && pkg upgrade
+pkg install cacafire
+cacafire
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+figlet "Loading.."
+sleep 2
+pkg update && pkg upgrade
+pkg install irssi
+irssi
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+figlet "Loading.."
+sleep 2
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/XvnAnony/X-Network
+cd X-Network
+sh X-Network.sh
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+figlet "Loading.."
+sleep 2
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "Loading.."
+sleep 2
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/muhammadfathul/VIRTEX
+cd VIRTEX
+sh virtex.sh
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+figlet "Loading.."
+pkg update && pkg upgrade
+pkg install python2
+git clone https://github.com/BangDanz/Darksimpel
+cd Darksimpel
+python2 dark.py
+fi
+
+if [ $bro = 0 ] || [ $bro = ^C ]
+then
+clear
+sleep 2
+echo $red
+figlet "Xvn"
+echo "Makasih bro udah mampir"
+sleep 2
+exit
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+figlet "Loading.."
+pkg update && pkg upgrade
+pkg install nodejs -y
+pkg install git
+git clone https://github.com/arief125/igtools
+cd igtools
+pkg install unzip
+ls
+unzip node_modules.zip
+npm install
+node index
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+figlet "Loading.."
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/novalattasya/tool-N
+cd tool-N
+python2 v1.py
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+figlet "Loading.."
+pkg update && pkg upgrade
+pkg install git
+pkg install python && pkg install python2
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+ls
+python2 LITEDDOS.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+figlet "Loading.."
+pkg update && pkg upgrade
+pkg install git
+pkg install bash
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+figlet "Loading.."
+pkg update && pkg upgrade
+pkg install neofetch
+neofetch
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+figlet "Loading.."
+sleep 2
+clear
+echo $green
+figlet "Tebak Tebakan"
+echo $white"Apa Yang Kamu Lakukan Di Saat Kamu Sedang Berdua Dengan Cewe Di Tempat Sepi"
+echo
+echo $green"A) Ajak Ngaji"
+echo $red"B) Di Ewe:v"
+echo $yellow
+read -p "Silahkan Dipilih: " bos
+fi
+
+if [ $bos = A ] || [ $bos = a ]
+then
+clear
+figlet "selamat"
+echo "Anda Mendapatkan 1.000.000 di potong pajak 1.000.000:v"
+sleep 3
+sh Demonizedv2.sh
+fi
+
+if [ $bos = B ] || [ $bos = b ]
+then
+clear
+figlet "Astghfiruwlah"
+echo "tobat lah nak kiamat sudah dekat:)"
+sleep 3
+sh Demonizedv2.sh
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+figlet "Loading..."
+pkg upgrade && pkg update
+pkg install git && pkg install bash
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+figlet "Loading.."
+pkg update && pkg upgrade
+pkg install python && pkg install python2
+pkg install git && pkg install bash
+git clone https://github.com/Ranginang67/DarkFly-Tool
+cd DarkFly-Tool
+python2 install.py
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+figlet "Loading.."
+pkg install php
+pkg upgrade && pkg update
+pkg install git
+pkg install bash
+pkg install python2
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+figlet "Loading.."
+pkg update && pkg upgrade
+pkg install git
+pkg install bash
+pkg install python2
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
